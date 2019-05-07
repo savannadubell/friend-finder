a8f4c8a9a9365a103dc0ad7176d51f0d17fbec03
Savanna Dubell <savannadubell@Savannas-MacBook-Pro-2.local>
2019-05-07 15:49:01 -0600

    first commit

---
+++ app/data/friend.js
@@ -0,0 +1,162 @@
+var friendsArray = [
+	{
+		name: 'Rachel Green',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRktDEZY_o8Bv2qb76xxazqjq3iPpXKXza4_Hkf1lzpsD51NrU6',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ross Geller',
+		photo: 'https://redditupvoted.files.wordpress.com/2015/10/ross-gellernew.jpg?w=1200&h=580&crop=1',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Monica Geller',
+		photo: 'https://imgix.bustle.com/rehost/2016/9/13/04a388e1-335b-4fad-969f-383866bddab7.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Chandler Bing',
+		photo: 'https://qph.fs.quoracdn.net/main-qimg-7f5b51550630457f9eabd0a37db16ad3',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Joey Trribbiani',
+		photo: 'https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE1XFxcLzEwXFxcLzI5MTExMzEyXFxcL0pvZXkuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_aWQ9MjY0YTJkYmUzNzBmMmM2NzVmY2RcIixcIm9wdGlvbnNcIjpbXX0iLCJoYXNoIjoiMDY2ODU4NGY3MDNjZjg1MWJiOWU1YTgxMDJlNWE4ZTUxMzBlNGI1MiJ9/joey.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Pheobe Buffay',
+		photo: 'http://www.comedycentral.co.uk/sites/default/files/styles/image-w-520-h-520-scale/public/cc_uk/galleries/large/2018/01/22/26.jpg?itok=V0B2m9Jo',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Meredith Gray',
+		photo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ellen-pompeo-meredith-grey-greys-anatomy-1551353336.jpg?crop=0.677xw:0.677xh;0.155xw,0.116xh&resize=480:*',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Derek Shepherd',
+		photo: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2016%2F08%2Fpatrick-dempsey-04-1024-1.jpg&w=400&c=sc&poi=face&q=85',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Christina Yang',
+		photo: 'https://pbs.twimg.com/profile_images/1948037859/GA_CO_717__30_-1.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Alex Karev',
+		photo: 'https://vignette.wikia.nocookie.net/greys/images/7/7d/AlexGAS10.jpg/revision/latest?cb=20130913153841',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
